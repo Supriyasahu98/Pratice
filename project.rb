e42d62aca410218240b222e2dae3204070f3f795
supriya sahu <nishisahu53@gmail.com>
2018-10-12 12:10:25 +0530

    water

---
+++ project.rb
@@ -0,0 +1,10 @@
+hungry=input("are you hungry")
+if hungry="yes"
+puts("eat samosa")
+puts("eat spizza")
+puts("eat burger")
+else
+	thirsty =input("are yot thirsty")
+	if thirsty="yes"
+
+puts("drink water")
